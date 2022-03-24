b92abf2635eb8742197317047d7a20ebfd866918
ignaHeinz <ignacio.heinzmann@gmail.com>
2022-03-24 10:16:21 -0300

    fix endpoint

--- public/js/app.js
+++ public/js/app.js
@@ -4,12 +4,13 @@ const messageOne = document.getElementById('message-1')
 const messageTwo = document.getElementById('message-2')
 messageOne.textContent = ''
 
+
 weatherForm.addEventListener('submit', (e) => {
     e.preventDefault()
     const location = search.value
     messageOne.textContent = 'Loading...'
     messageTwo.textContent = ''
-    fetch('http://localhost:3000/weather?address='+location).then((res) => {
+    fetch('/weather?address='+location).then((res) => {
     res.json().then(data => {
         if(!data.error){
             messageOne.textContent = data.location
